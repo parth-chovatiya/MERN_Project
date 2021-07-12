ee84ae836585ba5bf1e483d3b2725690df05609b
parth-chovatiya <parthchovatiya3@gmail.com>
2021-07-12 18:02:02 +0530

    improve

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -8,25 +8,28 @@ import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
-  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
   const openDrawerHandler = () => {
-    setDrawerIsOpen(true)
-  }
+    setDrawerIsOpen(true);
+  };
   const closeDrawerHandler = () => {
-    setDrawerIsOpen(false)
-  }
+    setDrawerIsOpen(false);
+  };
 
   return (
     <>
-      { drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
       <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
